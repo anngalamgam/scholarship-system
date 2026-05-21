aac516612f41ab67f348c666c28260f73685f66f
Rouin <anngalamgam@gmail.com>
2026-05-22 00:33:56 +0800

    fix welcome blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
 <style>
 
     *{
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
           href="{{ asset('build/assets/new/css/styles.css')}}">
 
     <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <style>
 
